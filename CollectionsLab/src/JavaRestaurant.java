c12190451699629dd40afb3e908022bb3af1c713
user <elvin.ortiz3@upr.edu>
2018-11-11 00:23:06 -0800

    First excercise done

--- CollectionsLab/src/JavaRestaurant.java
+++ CollectionsLab/src/JavaRestaurant.java
@@ -3,12 +3,56 @@ import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class JavaRestaurant {
+	
+	public static void main(String[] args) {
+		JavaRestaurant f = new JavaRestaurant();
+		f.CustomerManagement(new Stack<>());
+	}
 
 	public int CustomerManagement(Stack<Customer> SList) {
+		int t = 0; //time
+		int TotalMoney = 0; //money earned
+		Stack<Customer> clerk = new Stack<>(); //where the customer will be attended
+		Stack<Customer> WaitingLine = new Stack<>(); //waiting Line
+
+		//Looks for first customer
+		while (t != SList.peek().getTimeEntered()) {
+			t++;
+		}
+
+		//Push CLient to clerk
+		clerk.push(SList.pop());
+
+		//If initial list, the waiting line and the clerk aren't empty.
+		while (!SList.isEmpty() || !WaitingLine.isEmpty() || !clerk.isEmpty()) {
+
+			//If Clerk is available and someone is waiting in line, 
+			//push customer from waiting line to clerk
+			if (clerk.isEmpty() && !WaitingLine.isEmpty()) {
+				clerk.push(WaitingLine.pop());
+			}
+
+			//Looks if someone enter the restaurant
+			if (!SList.isEmpty() && SList.peek().timeEntered==t) {
+				WaitingLine.push(SList.pop());
+			}
+
+			//Takes 1 unit of time from customer in clerk for their respective order
+			clerk.peek().setTimeToCompleted(clerk.peek().getTimeToCompleted()-1);
+
+			//Verifies if the order is done
+			if (clerk.peek().getTimeToCompleted()<=0) {
+				TotalMoney += clerk.peek().getBill();
+				clerk.pop();
+			}
+			
+			t++;
+
+			
+		}
 
 
-
-		return 0;
+		return TotalMoney;
 
 	}
 
@@ -38,13 +82,25 @@ public class JavaRestaurant {
 		public int bill;
 		public int timeToCompleted;
 		public int Pacience;
+		public String Name;
 
 
-		public Customer(int timeEntered, int bill, int timeToCompleted, int Pacience) {
+		public Customer(String Name,int timeEntered, int bill, int timeToCompleted, int Pacience) {
 			this.timeEntered = timeEntered;
 			this.bill = bill;
 			this.timeToCompleted = timeToCompleted;
-			this.Pacience = Pacience;		
+			this.Pacience = Pacience;
+			this.Name = Name;
+		}
+
+
+		public String getName() {
+			return Name;
+		}
+
+
+		public void setName(String name) {
+			Name = name;
 		}
 
 
@@ -85,6 +141,10 @@ public class JavaRestaurant {
 
 		public void setPacience(int pacience) {
 			Pacience = pacience;
+		}
+		
+		public String toString() {
+			return "Name: " +Name + " Arrive: " + timeEntered + " \nOrders Time: " + timeToCompleted;
 		}
 
 
