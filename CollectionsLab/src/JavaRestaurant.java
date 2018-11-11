a7f657096106b167166623153e0391f4e3ed13de
user <elvin.ortiz3@upr.edu>
2018-11-10 19:30:21 -0800

    Set up

--- CollectionsLab/src/JavaRestaurant.java
+++ CollectionsLab/src/JavaRestaurant.java
@@ -1,4 +1,97 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class JavaRestaurant {
+
+	public int CustomerManagement(Stack<Customer> SList) {
+
+
+
+		return 0;
+
+	}
+
+	public int CustomerManagement(PriorityQueue<Customer> QList) {
+
+
+
+		return 0;
+
+	}
+
+	public int CustomerManagement(ArrayList<Customer> AList) {
+
+
+
+		return 0;
+
+	}
+
+
+
+
+
+
+	public class Customer {
+		public int timeEntered;
+		public int bill;
+		public int timeToCompleted;
+		public int Pacience;
+
+
+		public Customer(int timeEntered, int bill, int timeToCompleted, int Pacience) {
+			this.timeEntered = timeEntered;
+			this.bill = bill;
+			this.timeToCompleted = timeToCompleted;
+			this.Pacience = Pacience;		
+		}
+
+
+		public int getTimeEntered() {
+			return timeEntered;
+		}
+
+
+		public void setTimeEntered(int timeEntered) {
+			this.timeEntered = timeEntered;
+		}
+
+
+		public int getBill() {
+			return bill;
+		}
+
+
+		public void setBill(int bill) {
+			this.bill = bill;
+		}
+
+
+		public int getTimeToCompleted() {
+			return timeToCompleted;
+		}
+
+
+		public void setTimeToCompleted(int timeToCompleted) {
+			this.timeToCompleted = timeToCompleted;
+		}
+
+
+		public int getPacience() {
+			return Pacience;
+		}
+
+
+		public void setPacience(int pacience) {
+			Pacience = pacience;
+		}
+
+
+
+	}
+
+
+
 
 }
