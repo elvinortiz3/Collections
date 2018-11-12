17204b083d676a399813e49be8a0c91ac5905de3
user <elvin.ortiz3@upr.edu>
2018-11-12 00:25:15 -0800

    Added comparable exercise

--- CollectionsLab/src/JavaRestaurant.java
+++ CollectionsLab/src/JavaRestaurant.java
@@ -1,15 +1,27 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class JavaRestaurant {
-	
+
 	public static void main(String[] args) {
 		JavaRestaurant f = new JavaRestaurant();
-		f.CustomerManagement(new Stack<>());
+		//	f.CustomerManagement(new Stack<>());
+		//	f.CustomerManagement(new PriorityQueue<>());
+		f.CustomerManagement(new ArrayList<>());
 	}
 
+	//Exercise 1
+	//In this exercise you want to simulate a restaurant waiting line.
+	//Time is denoted by a variable "t" that starts at 0
+	//While the stack SList, waitingLine and clerk are not empty
+	//
+	//
+	//
 	public int CustomerManagement(Stack<Customer> SList) {
+	
+/////////////////////////Dar a los estudiantes desde aqui///////////////////////////////
 		int t = 0; //time
 		int TotalMoney = 0; //money earned
 		Stack<Customer> clerk = new Stack<>(); //where the customer will be attended
@@ -25,14 +37,17 @@ public class JavaRestaurant {
 
 		//If initial list, the waiting line and the clerk aren't empty.
 		while (!SList.isEmpty() || !WaitingLine.isEmpty() || !clerk.isEmpty()) {
-
+/////////////////////////Dar a los estudiantes hasta aqui///////////////////////////////
+			
 			//If Clerk is available and someone is waiting in line, 
 			//push customer from waiting line to clerk
 			if (clerk.isEmpty() && !WaitingLine.isEmpty()) {
 				clerk.push(WaitingLine.pop());
 			}
 
-			//Looks if someone enter the restaurant
+			//Looks if someone enter the restaurant from SList
+			//If time entered from top of stack equals t, then
+			//the customer must go to the waiting line
 			if (!SList.isEmpty() && SList.peek().timeEntered==t) {
 				WaitingLine.push(SList.pop());
 			}
@@ -41,20 +56,20 @@ public class JavaRestaurant {
 			clerk.peek().setTimeToCompleted(clerk.peek().getTimeToCompleted()-1);
 
 			//Verifies if the order is done
+			//if Orders is done, TotalMoney counter acumulates
 			if (clerk.peek().getTimeToCompleted()<=0) {
 				TotalMoney += clerk.peek().getBill();
 				clerk.pop();
 			}
-			
+
 			t++;
 
-			
+
 		}
-
-
 		return TotalMoney;
 
 	}
+
 
 	public int CustomerManagement(PriorityQueue<Customer> QList) {
 
@@ -64,11 +79,24 @@ public class JavaRestaurant {
 
 	}
 
-	public int CustomerManagement(ArrayList<Customer> AList) {
+	//Exercise 2
+	//A lot of people have entered the restaurant and you don't 
+	//know the order they have enter. Sort the Arraylist to attend
+	//the customers by the orders that take less time.
+	//Hint: Use the compareTo method in Customer class and Collections.sort();
+	public ArrayList<Customer> CustomerManagement(ArrayList<Customer> AList) {
+
+		ArrayList<Customer> l = new ArrayList<>();
+
+		l.add(new Customer("A", 0, 3, 7, 9));
+		l.add(new Customer("B", 0, 3, 1, 9));
+		l.add(new Customer("C", 0, 3, 6, 9));
+		l.add(new Customer("D", 0, 3, 9, 9));
+
+		Collections.sort(l);
 
 
-
-		return 0;
+		return l;
 
 	}
 
@@ -77,7 +105,7 @@ public class JavaRestaurant {
 
 
 
-	public class Customer {
+	public class Customer implements Comparable<Customer> {
 		public int timeEntered;
 		public int bill;
 		public int timeToCompleted;
@@ -142,16 +170,25 @@ public class JavaRestaurant {
 		public void setPacience(int pacience) {
 			Pacience = pacience;
 		}
-		
+
 		public String toString() {
 			return "Name: " +Name + " Arrive: " + timeEntered + " \nOrders Time: " + timeToCompleted;
 		}
 
 
+		//Implement the comparable so that 
+		//we can sort the Arraylist line by time completed
+		public int compareTo(Customer o) {		
+			if (this.getTimeToCompleted()>o.getTimeToCompleted()) {
+				return 1;
+			}
 
+			else {
+				return -1;
+			}
+		}
+		
+		
+		
 	}
-
-
-
-
 }
