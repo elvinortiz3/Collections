69a59629d6b2339d3288f8e81ce299e25878bd34
user <elvin.ortiz3@upr.edu>
2018-11-17 16:18:35 -0400

    Fixed some bugs

--- CollectionsLab/src/JavaRestaurant.java
+++ CollectionsLab/src/JavaRestaurant.java
@@ -108,7 +108,7 @@ public class JavaRestaurant {
 		/////////////////////////Dar a los estudiantes desde aqui///////////////////////////////
 		int t = 0; //time
 		int TotalMoney = 0; //money earned
-		Queue<Customer> clerk = new LinkedList<>(); //where the customer will be attended
+		Queue<Customer> cashRegister = new LinkedList<>(); //where the customer will be attended
 		Queue<Customer> WaitingLine = new LinkedList<>(); //waiting Line
 
 		//Looks for first customer
@@ -117,16 +117,16 @@ public class JavaRestaurant {
 		}
 
 		//Push CLient to clerk
-		clerk.add(queue.poll());
+		cashRegister.add(queue.poll());
 
 		//If initial list, the waiting line and the clerk aren't empty.
-		while (!queue.isEmpty() || !WaitingLine.isEmpty() || !clerk.isEmpty()) {
+		while (!queue.isEmpty() || !WaitingLine.isEmpty() || !cashRegister.isEmpty()) {
 			/////////////////////////Dar a los estudiantes hasta aqui///////////////////////////////
 
 			//If Clerk is available and someone is waiting in line, 
 			//push customer from waiting line to clerk
-			if (clerk.isEmpty() && !WaitingLine.isEmpty()) {
-				clerk.add(WaitingLine.poll());
+			if (cashRegister.isEmpty() && !WaitingLine.isEmpty()) {
+				cashRegister.add(WaitingLine.poll());
 			}
 
 			//Looks if someone enter the restaurant from SList
@@ -137,14 +137,14 @@ public class JavaRestaurant {
 			}
 
 			//Takes 1 unit of time from customer in clerk for their respective order
-			clerk.peek().setTimeToCompleted(clerk.peek().getTimeToCompleted()-1);
+			cashRegister.peek().setTimeToCompleted(cashRegister.peek().getTimeToCompleted()-1);
 
 			WaitingLine = pacienceReducer(WaitingLine);
 			//Verifies if the order is done
 			//if Orders is done, TotalMoney counter acumulates
-			if (clerk.peek().getTimeToCompleted()<=0) {
-				TotalMoney += clerk.peek().getBill();
-				clerk.poll();
+			if (cashRegister.peek().getTimeToCompleted()<=0) {
+				TotalMoney += cashRegister.peek().getBill();
+				cashRegister.poll();
 			}
 
 			t++;
@@ -156,13 +156,17 @@ public class JavaRestaurant {
 	}
 
 
+	//Exercise 3
+	//Implement a method to reduce 1 unit of time to every
+	//customer in the waiting line to his/hers patience.
+	//If patience is equals to zero, the customer will go.
 	private Queue<Customer> pacienceReducer(Queue<Customer> waitingLine) {
 
 		Queue<Customer> temp = new LinkedList<>();
 
 		while (!waitingLine.isEmpty()) {
-			waitingLine.peek().setPacience(waitingLine.peek().getPacience()-1);
-			if (waitingLine.peek().getPacience()==0) {
+			waitingLine.peek().setPatience(waitingLine.peek().getPatience()-1);
+			if (waitingLine.peek().getPatience()==0) {
 				waitingLine.poll();
 			}
 			else {
@@ -182,7 +186,7 @@ public class JavaRestaurant {
 
 
 
-	//Exercise 2
+	//Exercise 3
 	//A lot of people have entered the restaurant and you don't 
 	//know the order they have enter. Sort the Arraylist to attend
 	//the customers by the orders that take less time.
@@ -211,7 +215,7 @@ public class JavaRestaurant {
 		public int timeEntered;
 		public int bill;
 		public int timeToCompleted;
-		public int Pacience;
+		public int patience;
 		public String Name;
 
 
@@ -219,7 +223,7 @@ public class JavaRestaurant {
 			this.timeEntered = timeEntered;
 			this.bill = bill;
 			this.timeToCompleted = timeToCompleted;
-			this.Pacience = Pacience;
+			this.patience = Pacience;
 			this.Name = Name;
 		}
 
@@ -264,13 +268,13 @@ public class JavaRestaurant {
 		}
 
 
-		public int getPacience() {
-			return Pacience;
+		public int getPatience() {
+			return patience;
 		}
 
 
-		public void setPacience(int pacience) {
-			Pacience = pacience;
+		public void setPatience(int pacience) {
+			patience = pacience;
 		}
 
 		public String toString() {
@@ -281,7 +285,7 @@ public class JavaRestaurant {
 		//Implement the comparable so that 
 		//we can sort the Arraylist line by time completed
 		public int compareTo(Customer o) {		
-			if (this.getPacience()>o.getPacience()) {
+			if (this.getPatience()>o.getPatience()) {
 				return 1;
 			}
 
